0d480510baac0a6cc14a946fdae2baa9a67c2363
iam3kdeveloper <kamranktk807@gmail.com>
2021-09-21 04:14:49 -0700

    initialize Hive in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 import 'package:learn_hive_crud/views/home/home.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final appDocumentDirectory =
+      await path_provider.getApplicationDocumentsDirectory();
+  Hive.init(appDocumentDirectory.path);
   runApp(const MyApp());
 }
 
