02d2573b1de8dd1625101f644249565b49adf91d
iam3kdeveloper <kamranktk807@gmail.com>
2021-09-20 19:05:13 -0700

    adding hive related dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:hive_crud/views/home/home.dart';
+import 'package:learn_hive_crud/views/home/home.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setEnabledSystemUIOverlays(
-      [SystemUiOverlay.top, SystemUiOverlay.bottom]);
   runApp(const MyApp());
 }
 
